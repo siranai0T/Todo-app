a445168b55749868f6ef8a6dd0c672634c995bc2
saitou.r <saitou.r@lateral-t.co.jp>
2022-08-12 09:37:53 +0900

    論理削除

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['title', 'content','deadline','completion_date','status_id'];
+    protected $dates = ['deleted_at'];
 
     public function status()
     {
